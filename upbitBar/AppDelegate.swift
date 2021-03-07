fc328f5a512f5771f2483e0369e6a98f2eb5da2c
Fernando <fernando@dano.me>
2021-03-07 23:51:17 +0900

    Initial Commit

---
+++ upbitBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  upbitBar
+//
+//  Created by Fernando on 2021/03/07.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
