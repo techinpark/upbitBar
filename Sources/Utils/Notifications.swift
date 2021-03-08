2c39088aa7a1733cf5d3e94ef31fdeef3b594a2c
Fernando <fernando@dano.me>
2021-03-09 01:32:57 +0900

    ✨ Added Notification name extension

---
+++ Sources/Utils/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  upbitBar
+//
+//  Created by Fernando on 2021/03/09.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let neededTokenSetting = Notification.Name("neededTokenSetting")
+    static let neededRefresh = Notification.Name("neededRefresh")
+}
